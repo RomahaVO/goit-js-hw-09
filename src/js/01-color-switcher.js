11235deec928c76d31c042a3d2f18d9b7fdfe845
RomahaVO <romahabodnaruk@icloud.com>
2023-02-03 20:43:43 +0200

    exercise01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyForBgc = document.body;
+console.log(stopBtn);
 
+let timerId = null;
+// startBtn.disabled = false;
+// stoptBtn.disabled = true;
+
+function changeBgcBody () {
+    startBtn.disabled = true;
+    // stopBtn.disabled = false;
+    timerId = setInterval(() => {
+    bodyForBgc.style.backgroundColor = getRandomHexColor();
+
+},1000)};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', changeBgcBody);
+
+function stopChangeBgcBode() {
+    clearInterval(timerId);
+    // stoptBtn.disabled = true;
+    startBtn.disabled = false;
+
+};
+
+stopBtn.addEventListener('click', stopChangeBgcBode);
